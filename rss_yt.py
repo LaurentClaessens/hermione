dbf76c2580c849b26c55319acf0966a7d19b3c32
Laurent Claessens <laurent@claessens-donadello.eu>
2022-11-15 07:06:24 +0100

    (rss) Deduce the rss url from a video.

--- rss_yt.py
+++ rss_yt.py
@@ -1,13 +1,18 @@
 #!venv/bin/python3
 
 import sys
+import youtube_dl
 
-"""Provide the RSS link of a youtube channel."""
+"""Return the RSS url from the url of a video."""
 
 
 def do_work():
     """Make the work."""
-    channel = sys.argv[1]
-    print(f'https://www.youtube.com/feeds/videos.xml?channel_id={channel}')
+    url = sys.argv[1]
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        infos = ydl.extract_info(url, download=False)
+        channel_id = infos['channel_id']
+    print(f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}')
 
 do_work()
