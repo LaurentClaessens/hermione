c4d9c03aed01a1cdb3e8733022f26248de5ddd54
Laurent Claessens <laurent@claessens-donadello.eu>
2023-05-09 19:24:46 +0200

    (venv)

--- make_venv.sh
+++ make_venv.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 #
-# This script installs the required venv for the linter.
-#
 
-# apt install  make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
-
-
+# sudo apt install  build-essential zlib1g-dev libffi-dev libssl-dev libreadline-dev libsqlite3-dev liblzma-dev libbz2-dev
 # git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-# cd ~/.pyenv/libexec
-# ./pyenv install 3.7.4
-
-# You may have to hack
-# cd /usr/lib/x86_64-linux-gnu
-# sudo ln -s libffi.so.7 libffi.so.6
+# ~/.pyenv/bin/pyenv install -v 3.10.6
 
 set -u
 
-VERSION=3.8.3
+VERSION=3.10.6
 PYTHON3=~/.pyenv/versions/$VERSION/bin/python3
 MAIN_DIR=$PWD
 VENV_DIR=$MAIN_DIR/venv
