139c75c3757d62d75b4b0816c91bffaf219c3134
Laurent Claessens <laurent@claessens-donadello.eu>
2024-12-18 06:04:14 +0100

    (dl_from_file) Fix a FileNotFoundError occuring when the script reads the file in the same time as the user is writting it.

--- dl_from_file.py
+++ dl_from_file.py
@@ -4,6 +4,7 @@
 
 import sys
 import time
+import contextlib
 from concurrent.futures import ThreadPoolExecutor
 
 import dirmanage
@@ -17,6 +18,7 @@ class Options:
 
     def __init__(self):
         self.already_submited: list[str] = []
+        self.finished: list[str] = []
         self.list_file = dirmanage.init_dir / sys.argv[1]
         filename = f"with_errors_{random_string(5)}.json"
         self.error_file = dirmanage.init_dir / filename
@@ -27,9 +29,24 @@ class Options:
         write_json_file(self.with_error, self.error_file)
 
 
+def read_list_file(options: Options):
+    """
+    Return the content of the text file we are following.
+
+    Since the point of this script is to write in the file in the same 
+    time as reading, it happens that the file is being written when
+    we read it. This causes FileNotFoundError.
+    """
+    text = None
+    while text is None:
+        with contextlib.suppress(FileNotFoundError):
+            text = options.list_file.read_text()
+    return text
+
+
 def get_new_urls(options: Options):
     """yield a list of new urls."""
-    text = options.list_file.read_text()
+    text = read_list_file(options)
     lines = text.splitlines()
     for line in lines:
         if line not in options.already_submited:
@@ -62,3 +79,4 @@ with ThreadPoolExecutor(max_workers=5) as executor:
             options.already_submited.append(url)
             job = executor.submit(one_job, url)
         time.sleep(1)
+        print(f"Done: {options.finished}/{options.already_submited}")
